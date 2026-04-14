50c59fe926edbb2b682fcb453403175963ae888f
milkcandy <milkcandyxxxx@gmail.com>
2026-04-14 22:41:44 +0800

    增加plugins存储插件而非单个匹配器，新增插件信息实现。

---
+++ plugin/plugin.go
@@ -0,0 +1,43 @@
+/**
+ * @author milkcandy
+ * @date 2026/4/14
+ * @description TODO
+ */
+
+package plugin
+
+import (
+	"log"
+	"sync"
+)
+
+// Plugin 插件总结构体储存信息
+type Plugin struct {
+	Name    string
+	Version string
+	Author  string
+	Help    string
+	Matcher []Matcher
+}
+
+// // Info 插件基础信息结构体
+// type Info struct {
+//
+// }
+var (
+	plugins       = []*Plugin{} // 存储所有的匹配器注册
+	runningPlugin *Plugin
+	mu            sync.RWMutex // 加锁（目前是冷加载插件，后续热加载等需要注意）
+)
+
+// addPlugin 添加插件
+func addPlugin(p *Plugin) {
+	mu.Lock()
+	defer mu.Unlock()
+	plugins = append(plugins, p)
+	runningPlugin = p
+	log.Println("\n加载插件:", p.Name, "\n版本:", p.Version, "\n作者:", p.Author)
+}
+func GetPluginName() string {
+	return runningPlugin.Name
+}
