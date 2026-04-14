839303d7756ce6a4bed3a2c69d2cda2d91fdbcbe
milkcandy <milkcandyxxxx@gmail.com>
2026-04-14 22:41:32 +0800

    新增插件信息实现

--- plugins/ping.go
+++ plugins/ping.go
@@ -6,9 +6,14 @@
 
 package plugins
 
-import "github.com/milkcandyxxxx/Kumobot/plugin"
+import (
+	"github.com/milkcandyxxxx/Kumobot/plugin"
+	"log"
+)
 
 func init() {
+	plugin.OnPlugin("ping", "0.0.1", "milkcandy", "pong")
+	log.Println(plugin.GetPluginName())
 	// 指定词语回复
 	plugin.OnCommand("ping", func(ctx *plugin.Ctx) {
 		ctx.Send("pong")
