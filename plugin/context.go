1c45a9815f1a7185f31e77fe8658702cbf2b489c
milkcandy <milkcandyxxxx@gmail.com>
2026-04-15 22:51:52 +0800

    优化存储内置函数位置

--- plugin/context.go
+++ plugin/context.go
@@ -9,7 +9,6 @@ package plugin
 import (
 	"github.com/milkcandyxxxx/Kumobot/adapter"
 	"github.com/milkcandyxxxx/Kumobot/core"
-	"strings"
 )
 
 // TODO 目前还未直线自动配置（适配器等）
@@ -26,19 +25,4 @@ type Ctx struct {
 	event   *core.Event // 原始事件
 	message string      // 消息纯文本
 	bot     *Bot        // 机器人实例
-}
-
-func (c *Ctx) Send(msg string) error {
-	if c.event.DetailType == "private" {
-		return c.bot.SendPrivateMessage(c.event.UserID, msg)
-	}
-	if c.event.DetailType == "channel" {
-		return c.bot.SendGroupMessage(c.event.GroupID, msg)
-	}
-	return nil
-}
-
-// ExtractPlainText 用于获取第一个参数
-func (c *Ctx) ExtractPlainText() string {
-	return strings.SplitN(c.message, " ", 2)[1]
 }
